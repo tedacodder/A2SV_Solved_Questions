8df2f2280d9d1c0c39cd822fbcda6a4e0240ea77
tedacodder <155418230+tedacodder@users.noreply.github.com>
2026-02-04 05:18:00 -0800

    Add Weird.py to evaluate number weirdness

---
+++ py-if-else/Weird.py
@@ -0,0 +1,21 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    
+        
+    if n % 2 == 0:
+        if 6 <= n <= 20:
+            print("Weird")
+        else:
+            print("Not Weird")
+    else:
+        print("Weird")
