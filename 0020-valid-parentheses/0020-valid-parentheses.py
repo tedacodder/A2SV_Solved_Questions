3de9cf06d055e9e5f848ee82451616b5700bdcf7
tedacodder <155418230+tedacodder@users.noreply.github.com>
2026-03-09 18:27:26 +0300

    Time: 0 ms (100%), Space: 19.4 MB (26.3%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        dic={"}":"{",")":"(","]":"["}
+        stack=[]
+        for i in range(len(s)):
+            if stack and s[i] in dic and stack[-1]==dic[s[i]]:
+                stack.pop()
+                continue
+            stack.append(s[i])
+        return len(stack)==0
+
