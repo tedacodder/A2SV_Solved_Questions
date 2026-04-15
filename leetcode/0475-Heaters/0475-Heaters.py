49b8fb2c056c6ab9eedf5c127497f58b417ccd71
tedacodder <155418230+tedacodder@users.noreply.github.com>
2026-04-15 19:26:18 +0300

    Add solution for Heaters

---
+++ leetcode/0475-Heaters/0475-Heaters.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findRadius(self, houses: List[int], heaters: List[int]) -> int:
+     
+        houses.sort()
+        heaters.sort()
+        radius = 0
+        for house in houses:
+            left, right = 0, len(heaters) - 1
+            while left < right:
+                mid = (left + right) // 2
+                if heaters[mid] < house:
+                    left = mid + 1
+                else:
+                    right = mid
+            dist = abs(heaters[left] - house)
+            if left > 0:
+                dist = min(dist, abs(heaters[left - 1] - house))
+            radius = max(radius, dist)
+        return radius
